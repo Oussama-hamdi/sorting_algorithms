da9f98138ffba613115e45caae2ad531ba0f15d0
root <ohamdi776@gmail.com>
2023-07-24 04:51:13 -0700

    Completed 2.Selection_sort task

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ * @array: Pointer to the array
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min_index, temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; ++i)
+	{
+		min_index = 1;
+		for (j = i + 1; j < size; ++j)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (min_index != (int)i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
+}
